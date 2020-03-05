a4ebd053cdfc95a34ebb17b34152404bd60c0a39
Kyle Huang Junyuan <kyle@hjunyuan.com>
2020-03-06 01:32:45 +0800

    Fixed a merge error

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-
 function App() {
 	return (
 		<Router>
