5df3e2f2875cc1f71d69dd2f464b563c8d6d3881
Guang Wei <guangwei.ngoh@gmail.com>
2020-03-05 17:55:42 +0800

    added landingcard component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import LandingCard from "./components/LandingCard";
 
 function App() {
-	return <div className="App">App</div>;
+  return (
+    <div className="App">
+      App
+      <br />
+      <LandingCard></LandingCard>
+    </div>
+  );
 }
 
 export default App;
