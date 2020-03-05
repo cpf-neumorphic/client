51aa32d7023cd75fd4c2aaa249fa0bf8a3a32e06
Kyle Huang Junyuan <kyle@hjunyuan.com>
2020-03-06 00:37:30 +0800

    Introduced Open Sans and Quicksand fonts

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
+
+import "typeface-quicksand";
+import "typeface-open-sans";
+
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
