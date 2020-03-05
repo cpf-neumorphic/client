b441a7e2d41ae33703c6f9d0a54a541745ea3fdd
Kyle Huang Junyuan <kyle@hjunyuan.com>
2020-03-05 23:38:45 +0800

    Completed Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 
+import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
 function App() {
 	return (
 		<>
+			<Navbar></Navbar>
 			<main>Main view</main>
 			<Footer></Footer>
 		</>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,43 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import styled from "styled-components";
+import Container from "react-bootstrap/Container";
+import CPFLogo from "../img/cpf-logo.png";
+
+const Nav = styled.nav`
+	background-color: white;
+`;
+
+const Navbar = styled(Container)`
+	display: flex;
+`;
+
+const NavItems = styled.div`
+	display: flex;
+	margin-left: auto;
+`;
+
+const Link = styled.a`
+	padding: 15px 10px;
+`;
 
 export default props => {
-	return <>Nav</>;
+	const name = props.name || "Adam";
+
+	return (
+		<Nav>
+			<Navbar>
+				<div>
+					<img src={CPFLogo} alt="CPFB Logo" style={{ height: "60px" }} />
+				</div>
+				<NavItems>
+					<Link>
+						<FontAwesomeIcon icon={faUser} style={{ marginRight: "10px" }} />
+						{name}
+					</Link>
+				</NavItems>
+			</Navbar>
+		</Nav>
+	);
 };
