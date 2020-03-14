3946db3db3bdcaaeb1c757f6a6cc96869a78b958
Kyle Huang Junyuan <kyle@hjunyuan.com>
2020-03-14 19:32:10 +0800

    Clean up

--- src/App.js
+++ src/App.js
@@ -27,108 +27,108 @@ export default function App() {
 			</div>
 		</Router>
 	);
+}
+
 const Button_ = styled(Button)`
-  // border-style: solid;
-  height: 310px;
-  width: 300px;
-  padding-top: 50px;
-  padding-right: 70px;
-  padding-bottom: 50px;
-  padding-left: 70px;
-  border-radius: 10%;
-  // overflow-y: hidden;
-  display: flex;
+	// border-style: solid;
+	height: 310px;
+	width: 300px;
+	padding-top: 50px;
+	padding-right: 70px;
+	padding-bottom: 50px;
+	padding-left: 70px;
+	border-radius: 10%;
+	// overflow-y: hidden;
+	display: flex;
 `;
 
 function App() {
-  let userFavourites = [];
-  let rows = 0;
-  let productRows = [];
+	let userFavourites = [];
+	let rows = 0;
+	let productRows = [];
 
-  const isDesktopOrLaptop = useMediaQuery({
-    query: "(min-width: 600px)"
-  });
-  const isTabletOrMobileDevice = useMediaQuery({
-    query: "(max-width: 600px)"
-  });
+	const isDesktopOrLaptop = useMediaQuery({
+		query: "(min-width: 600px)"
+	});
+	const isTabletOrMobileDevice = useMediaQuery({
+		query: "(max-width: 600px)"
+	});
 
-  const getUserFavourites = props => {
-    userFavourites = [];
-    rows = 0;
-    productRows = [];
-    console.log(props.Favourites);
+	const getUserFavourites = props => {
+		userFavourites = [];
+		rows = 0;
+		productRows = [];
+		console.log(props.Favourites);
 
-    props.Favourites.map(Favourite =>
-      Favourite.status ? userFavourites.push(Favourite) : ""
-    );
-    rows = [...Array(Math.ceil(userFavourites.length / 3))];
-    productRows = rows.map((row, idx) =>
-      userFavourites.slice(idx * 3, idx * 3 + 3)
-    );
-    // productRows.map(row => console.log(row));
-    // return productRows;
-  };
+		props.Favourites.map(Favourite =>
+			Favourite.status ? userFavourites.push(Favourite) : ""
+		);
+		rows = [...Array(Math.ceil(userFavourites.length / 3))];
+		productRows = rows.map((row, idx) =>
+			userFavourites.slice(idx * 3, idx * 3 + 3)
+		);
+		// productRows.map(row => console.log(row));
+		// return productRows;
+	};
 
-  const [Items, setItems] = useState({
-    Favourites: require("./asset/List_of_favourites.json")
-  });
+	const [Items, setItems] = useState({
+		Favourites: require("./asset/List_of_favourites.json")
+	});
 
-  console.log(Items.Favourites);
+	console.log(Items.Favourites);
 
-  return (
-    // <Styles>
+	return (
+		<Container>
+			{getUserFavourites(Items)}
+			<Row>
+				<h1>Favourites </h1>
+			</Row>
+			<Row>
+				<p>You mostly used these </p>
+			</Row>
 
-    <Container>
-      {getUserFavourites(Items)}
-      <Row>
-        <h1>Favourites </h1>
-      </Row>
-      <Row>
-        <p>You mostly used these </p>
-      </Row>
-
-      {isDesktopOrLaptop && (
-        <div>
-          {productRows.map((row, idx) => (
-            <Row className="m-4">
-              {row.map(Favourite => (
-                <Col sm={4}>
-                  <Button_ variant="light" className="shadow-sm m-1">
-                    <Favourites_card data={Favourite} size={"30%"} />
-                  </Button_>
-                </Col>
-              ))}
-              {productRows.length === idx + 1 ? (
-                <Favourites_addbutton data={setItems} Items={Items} />
-              ) : (
-                ""
-              )}
-            </Row>
-          ))}
-          {productRows.length === 0 ? (
-            <Favourites_addbutton data={setItems} Items={Items} />
-          ) : (
-            ""
-          )}
-        </div>
-      )}
-      {isTabletOrMobileDevice && (
-        <div className="mobile_config">
-          {userFavourites.map(Favourite => (
-            <Button_
-              id="mobile_favouritesCart"
-              variant="light"
-              className="shadow-sm m-2"
-            >
-              <Favourites_card data={Favourite} size={"30%"} />
-            </Button_>
-          ))}
-          <div className="mobile_Addbutton">
-            <Favourites_addbutton data={setItems} Items={Items} />
-          </div>
-        </div>
-      )}
-    </Container>
-    // </Styles>
-  );
+			{isDesktopOrLaptop && (
+				<div>
+					{productRows.map((row, idx) => (
+						<Row className="m-4">
+							{row.map(Favourite => (
+								<Col sm={4}>
+									<Button_ variant="light" className="shadow-sm m-1">
+										<Favourites_card data={Favourite} size={"30%"} />
+									</Button_>
+								</Col>
+							))}
+							{productRows.length === idx + 1 ? (
+								<Favourites_addbutton data={setItems} Items={Items} />
+							) : (
+								""
+							)}
+						</Row>
+					))}
+					{productRows.length === 0 ? (
+						<Favourites_addbutton data={setItems} Items={Items} />
+					) : (
+						""
+					)}
+				</div>
+			)}
+			{isTabletOrMobileDevice && (
+				<div className="mobile_config">
+					{userFavourites.map(Favourite => (
+						<Button_
+							id="mobile_favouritesCart"
+							variant="light"
+							className="shadow-sm m-2"
+						>
+							<Favourites_card data={Favourite} size={"30%"} />
+						</Button_>
+					))}
+					<div className="mobile_Addbutton">
+						<Favourites_addbutton data={setItems} Items={Items} />
+					</div>
+				</div>
+			)}
+		</Container>
+		// </Styles>
+	);
 }
