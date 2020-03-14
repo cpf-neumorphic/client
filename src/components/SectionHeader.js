658a675e27eb86e882cfd5bcb06eb1c36823ff81
Kyle Huang Junyuan <kyle@hjunyuan.com>
2020-03-14 12:10:18 +0800

    Wrap SectionHeader in a container

--- src/components/SectionHeader.js
+++ src/components/SectionHeader.js
@@ -2,9 +2,9 @@ import React from "react";
 
 export default props => {
 	return (
-		<>
+		<div className="container">
 			<h1>{props.header}</h1>
 			<p className="mb-4">{props.text}</p>
-		</>
+		</div>
 	);
 };
