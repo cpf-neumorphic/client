2a52221645fc3b1ff76860b3db324b042df23a83
Guang Wei <guangwei.ngoh@gmail.com>
2020-03-16 00:24:14 +0800

    added styles for search bar

--- src/components/SearchBar2.js
+++ src/components/SearchBar2.js
@@ -3,8 +3,8 @@ import React, { Component, useState } from 'react';
 import styled, { ThemeConsumer } from "styled-components";
 import Select from 'react-select';
 import makeAnimated from 'react-select/animated';
-import {Redirect} from 'react-router-dom';
 import {useHistory} from 'react-router-dom';
+import "./SearchBar2.css";
 
 const options = [
     {label: 'Education', value: 'education'},
@@ -13,18 +13,44 @@ const options = [
     {label: 'Statements', value: 'statements'},
 ];
 
-// const styledSelect = {
-//   color: '#000000',
-//   fontSize: '36px',
-//   width: '100px'
-// };
+const styledSelect = {
+  width: '400px',
+  display: 'inline-block',
+  alignItems: 'center',
+  justifyContent: 'center',
+  zIndex: '1000000'
+};
+
+// const styledSelect = styled(Select)`
+//   width: 500px;
+// `;
+
+const MultiSelect = styled(Select)`
+  z-index: 1000000;
+	&.Select--multi  {
+
+		.Select-value {
+			display: inline-flex;
+			align-items: left;
+		}		
+	}
+
+	& .Select-placeholder {
+		font-size: smaller;
+  }
+  
+  .react-select__menu {
+    height: 10px !important;
+  }
+`
 
 function customTheme(theme){
     return{
         ...theme,
         colors: {
             ...theme.colors,
-            primary25: '#89eff2',
+            // primary25: '#89eff2',
+            primary25: '#e4f9f5',
             primary: '#11999e',
         }
     }
@@ -32,17 +58,14 @@ function customTheme(theme){
 
 export default props => {
   const history = useHistory();
-    // const [feature, setFeature] = useState({});
-    // console.log(feature);
-    
   return (
     <>
-      {/* <SelectSearch options={optionss} value="None" name="language" placeholder="What are you looking for?" /> */}
-      {/* <Select options={options} styles={customStyles}/> */}
-    <Select options={options} theme={customTheme} placeholder="What are you looking for?" onChange={ (feature) => {
+      <div className="change" style={styledSelect}>
+    <MultiSelect className="selectup" options={options} theme={customTheme} placeholder="🔍 What are you looking for?" onChange={ (feature) => {
       console.log(feature.value);
       history.push("/"+feature.value)
     }}/>
+    </div>
     </>
   );
 };
