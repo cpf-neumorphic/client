fc3379e5f74aa59cefd8c47f1d244650ed8e4087
Kyle Huang Junyuan <kyle@hjunyuan.com>
2020-06-22 21:45:40 +0800

    Added .prettierrc file

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,70 +1,70 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
 
-import Scrollable from "../components/Scrollable";
-import SectionHeader from "../components/SectionHeader";
-import LandingCard from "../components/LandingCard";
-import SearchBar from "../components/SearchBar2";
-import Favourites from "../components/Favourites";
-import RecommendCard from "../components/RecommendCard";
+import Scrollable from '../components/Scrollable';
+import SectionHeader from '../components/SectionHeader';
+import LandingCard from '../components/LandingCard';
+import SearchBar from '../components/SearchBar2';
+import Favourites from '../components/Favourites';
+import RecommendCard from '../components/RecommendCard';
 // import SearchBar from "../components/SearchBar";
 
-import helloImg from "../img/landing-hello.png";
+import helloImg from '../img/landing-hello.png';
 
 const Section = styled.section`
-	margin-bottom: 4rem;
+  margin-bottom: 4rem;
 `;
 
 const LandingGreen = styled.div`
-	background: #e4f9f5;
-	padding-bottom: 160px;
-	margin-bottom: -160px;
-	text-align: center;
+  background: #e4f9f5;
+  padding-bottom: 160px;
+  margin-bottom: -160px;
+  text-align: center;
 `;
 
-export default props => {
-	const name = props.name || "Adam";
+export default (props) => {
+  const name = props.name || 'Adam';
 
-	return (
-		<>
-			<Section>
-				<LandingGreen>
-					<div className="container">
-						<img
-							style={{ height: "120px", margin: "2rem auto" }}
-							alt="Hello there"
-							src={helloImg}
-							draggable="false"
-						/>
-						<h1>Hello, {name}</h1>
-						<p>What would you like to do today?</p>
-						<SearchBar></SearchBar>
-					</div>
-				</LandingGreen>
-				<Scrollable center={1}>{landingCards}</Scrollable>
-			</Section>
-			<Section>
-				<SectionHeader header="Favourites" text="You mostly used these" />
-				<Scrollable>
-					<Favourites />
-				</Scrollable>
-			</Section>
-			<Section>
-				<SectionHeader header="Recommended" text="These may be useful" />
-				<Scrollable>
-					<RecommendCard />
-				</Scrollable>
-			</Section>
-		</>
-	);
+  return (
+    <>
+      <Section>
+        <LandingGreen>
+          <div className="container">
+            <img
+              style={{ height: '120px', margin: '2rem auto' }}
+              alt="Hello there"
+              src={helloImg}
+              draggable="false"
+            />
+            <h1>Hello, {name}</h1>
+            <p>What would you like to do today?</p>
+            <SearchBar></SearchBar>
+          </div>
+        </LandingGreen>
+        <Scrollable center={1}>{landingCards}</Scrollable>
+      </Section>
+      <Section>
+        <SectionHeader header="Favourites" text="You mostly used these" />
+        <Scrollable>
+          <Favourites />
+        </Scrollable>
+      </Section>
+      <Section>
+        <SectionHeader header="Recommended" text="These may be useful" />
+        <Scrollable>
+          <RecommendCard />
+        </Scrollable>
+      </Section>
+    </>
+  );
 };
 
 const landingCards = (
-	<>
-		<LandingCard icon="Zap" header="View" text="Your statements" />
-		<LandingCard icon="Bell" header="Manage" text="Your accounts" />
-		<LandingCard icon="Bell" header="Appointments" text="Create & modify" />
-	</>
+  <>
+    <LandingCard icon="Zap" header="View" text="Your statements" />
+    <LandingCard icon="Bell" header="Manage" text="Your accounts" />
+    <LandingCard icon="Bell" header="Appointments" text="Create & modify" />
+  </>
 );
 
 // const styles = {
