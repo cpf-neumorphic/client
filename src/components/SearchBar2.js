1bcbddcf37f461c92165b56933d53593bf186c09
Kyle Huang Junyuan <kyle@hjunyuan.com>
2020-06-22 20:14:49 +0800

    Minor UI tweaks

--- src/components/SearchBar2.js
+++ src/components/SearchBar2.js
@@ -1,24 +1,29 @@
 // import React from "react";
-import React, { Component, useState } from 'react';
+import React, { Component, useState } from "react";
 import styled, { ThemeConsumer } from "styled-components";
-import Select from 'react-select';
-import makeAnimated from 'react-select/animated';
-import {useHistory} from 'react-router-dom';
+import Select from "react-select";
+import makeAnimated from "react-select/animated";
+import { useHistory } from "react-router-dom";
 import "./SearchBar2.css";
 
 const options = [
-    {label: 'Education', value: 'education'},
-    {label: 'Housing', value: 'housing'},
-    {label: 'Investments', value: 'investments'},
-    {label: 'Statements', value: 'statements'},
+	{ label: "Education", value: "education" },
+	{ label: "Housing", value: "housing" },
+	{ label: "Investments", value: "investments" },
+	{ label: "Statements", value: "statements" },
 ];
 
 const styledSelect = {
-  width: '400px',
-  display: 'inline-block',
-  alignItems: 'center',
-  justifyContent: 'center',
-  zIndex: '1000000'
+	maxWidth: "400px",
+	margin: "0 auto",
+	zIndex: "1000000",
+};
+
+const customStyle = {
+	option: (provided, state) => ({
+		...provided,
+		textAlign: "left",
+	}),
 };
 
 // const styledSelect = styled(Select)`
@@ -26,46 +31,52 @@ const styledSelect = {
 // `;
 
 const MultiSelect = styled(Select)`
-  z-index: 1000000;
-	&.Select--multi  {
-
+	z-index: 1000000;
+	&.Select--multi {
 		.Select-value {
 			display: inline-flex;
 			align-items: left;
-		}		
+		}
 	}
 
 	& .Select-placeholder {
 		font-size: smaller;
-  }
-  
-  .react-select__menu {
-    height: 10px !important;
-  }
-`
+	}
 
-function customTheme(theme){
-    return{
-        ...theme,
-        colors: {
-            ...theme.colors,
-            // primary25: '#89eff2',
-            primary25: '#e4f9f5',
-            primary: '#11999e',
-        }
-    }
+	.react-select__menu {
+		height: 10px !important;
+	}
+`;
+
+function customTheme(theme) {
+	return {
+		...theme,
+		colors: {
+			...theme.colors,
+			// primary25: '#89eff2',
+			primary25: "#e4f9f5",
+			primary: "#11999e",
+		},
+	};
 }
 
-export default props => {
-  const history = useHistory();
-  return (
-    <>
-      <div className="change" style={styledSelect}>
-    <MultiSelect className="selectup" options={options} theme={customTheme} placeholder="🔍 What are you looking for?" onChange={ (feature) => {
-      console.log(feature.value);
-      history.push("/"+feature.value)
-    }}/>
-    </div>
-    </>
-  );
+export default (props) => {
+	const history = useHistory();
+	return (
+		<>
+			<div className="change" style={styledSelect}>
+				<MultiSelect
+					className="selectup"
+					options={options}
+					theme={customTheme}
+					placeholder="🔍 What are you looking for?"
+					onChange={(feature) => {
+						console.log(feature.value);
+						history.push("/" + feature.value);
+					}}
+					styles={customStyle}
+				/>
+			</div>
+		</>
+	);
 };
