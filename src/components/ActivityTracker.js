7c3a30e5da3f6b6953d93fbf5c102ebb2448aeb8
Kyle Huang Junyuan <kyle@hjunyuan.com>
2020-07-02 22:08:43 +0800

    Issue with idleTimer instance?

--- src/components/ActivityTracker.js
+++ src/components/ActivityTracker.js
@@ -1,94 +1,85 @@
-import React, { useContext, useState, useRef } from "react";
+import React, { useContext, useState, useEffect, useRef } from "react";
+import { useLocation } from "react-router-dom";
 import IdleTimer from "react-idle-timer";
 import { getPageIdFromSlug } from "../asset/pages";
 import axios from "axios";
 import { UserContext } from "../contexts/UserContext";
 
-const idleTimer = null;
+let idleTimer = null;
 
 export const ActivityTracker = (props) => {
+  const location = useLocation();
   const { currentUser } = useContext(UserContext);
-  const [curActiveState, setCurActiveState] = useState([
-    true,
-    window.location.pathname,
-  ]);
+  const [prevPathname, setPrevPathname] = useState(location.pathname);
 
   const timeout = 5000;
 
-  const _onAction = (currentUser, curActiveState, setCurActiveState) => {
-    console.log(curActiveState);
-    console.log(curActiveState[1] !== window.location.pathname);
-    console.log(curActiveState[1] === window.location.pathname);
-    if (curActiveState[0]) {
-      if (curActiveState[1] !== window.location.pathname) {
-        console.log("Different slug", idleTimer.getElapsedTime());
-        setCurActiveState([true, window.location.pathname]);
-        idleTimer.reset();
-      }
-    } else {
-      console.log("reset");
-      idleTimer.reset();
-      setCurActiveState([true, window.location.pathname]);
-    }
-  };
+  // Called whenever pathname changes
+  useEffect(() => {
+    // updateUsage(currentUser.nric, prevPathname);
+    console.log("Page changed: ", idleTimer.getElapsedTime());
+    idleTimer.reset();
+    setPrevPathname(location.pathname);
+  }, [location.pathname]);
 
-  const _onActive = (currentUser, curActiveState, setCurActiveState) => {
-    console.log(curActiveState);
-    console.log(curActiveState[1] !== window.location.pathname);
-    console.log(curActiveState[1] === window.location.pathname);
-    if (curActiveState[0]) {
-      if (curActiveState[1] !== window.location.pathname) {
-        console.log("Different slug", idleTimer.getElapsedTime());
-        setCurActiveState([true, window.location.pathname]);
-        idleTimer.reset();
-      }
-    } else {
-      console.log("reset");
-      idleTimer.reset();
-      setCurActiveState([true, window.location.pathname]);
-    }
-  };
+  // const _onAction = (currentUser, curActiveState, setCurActiveState) => {
+  //   console.log(curActiveState);
+  //   console.log(curActiveState[1] !== window.location.pathname);
+  //   console.log(curActiveState[1] === window.location.pathname);
+  //   if (curActiveState[0]) {
+  //     if (curActiveState[1] !== window.location.pathname) {
+  //       console.log("Different slug", idleTimer.getElapsedTime());
+  //       setCurActiveState([true, window.location.pathname]);
+  //       idleTimer.reset();
+  //     }
+  //   } else {
+  //     console.log("reset");
+  //     idleTimer.reset();
+  //     setCurActiveState([true, window.location.pathname]);
+  //   }
+  // };
 
-  /**
-   * POST user usage to API
-   * @param {object} data
-   */
-  const updateUsage = (currentUser) => {
-    let slug = window.location.pathname;
-    let page_idx = getPageIdFromSlug(slug);
-    let sendInfor = {
-      nric: currentUser.nric,
-      page_id: String(page_idx),
-      duration: String(idleTimer.getElapsedTime()),
-    };
-    axios
-      .post(
-        "http://13.67.40.27:3001/api/updateUsage",
-        JSON.stringify(sendInfor)
-      )
-      .then((response) => {
-        console.log(response);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
+  // const _onActive = (currentUser, curActiveState, setCurActiveState) => {
+  //   console.log(curActiveState);
+  //   console.log(curActiveState[1] !== window.location.pathname);
+  //   console.log(curActiveState[1] === window.location.pathname);
+  //   if (curActiveState[0]) {
+  //     if (curActiveState[1] !== window.location.pathname) {
+  //       console.log("Different slug", idleTimer.getElapsedTime());
+  //       setCurActiveState([true, window.location.pathname]);
+  //       idleTimer.reset();
+  //     }
+  //   } else {
+  //     console.log("reset");
+  //     idleTimer.reset();
+  //     setCurActiveState([true, window.location.pathname]);
+  //   }
+  // };
 
   /**
    * Trigger on idle more then 5 second.
-   * @param {object} currentUser
-   * @param {function} setCurActiveState
    */
-  const _onIdle = (currentUser, setCurActiveState) => {
-    // updateUsage(currentUser);
-    console.log("idle", idleTimer.getElapsedTime());
-    setCurActiveState([false, window.location.pathname]);
+  const onIdle = () => {
+    // updateUsage(currentUser.nric, location.pathname);
+    console.log("onIdle: Timer pause & reset -", idleTimer.getElapsedTime());
+  };
+
+  /**
+   * Trigger on active.
+   */
+  const onActive = () => {
+    console.log("onActive: Timer reset.");
     idleTimer.reset();
   };
 
-  let onAction = _onAction(currentUser, curActiveState, setCurActiveState);
-  let onActive = _onActive(currentUser, curActiveState, setCurActiveState);
-  let onIdle = _onIdle(currentUser, setCurActiveState);
+  // const onAction = () => {
+  //   console.log("onAction: Timer reset.");
+  //   idleTimer.resume();
+  // };
+
+  // let onAction = _onAction(currentUser, curActiveState, setCurActiveState);
+  // let onActive = _onActive(currentUser, curActiveState, setCurActiveState);
+
   return (
     <IdleTimer
       ref={(ref) => {
@@ -97,10 +88,32 @@ export const ActivityTracker = (props) => {
       element={document}
       onActive={onActive}
       onIdle={onIdle}
-      onAction={onAction}
+      // onAction={onAction}
       debounce={250}
       timeout={timeout}
-      stopOnIdle={false}
+      // stopOnIdle={true}
     />
   );
+};
+
+/**
+ * POST user usage to API
+ * @param {string} nric
+ * @param {string} pathname
+ */
+const updateUsage = (nric, pathname) => {
+  let page_idx = getPageIdFromSlug(pathname);
+  let sendInfor = {
+    nric: nric,
+    page_id: String(page_idx),
+    duration: String(idleTimer.getElapsedTime()),
+  };
+  axios
+    .post("http://13.67.40.27:3001/api/updateUsage", JSON.stringify(sendInfor))
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 };
