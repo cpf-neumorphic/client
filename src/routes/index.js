3b14c08910f2d98cbde659fb95a1bd0dd4cc60f2
Kyle Huang Junyuan <kyle@hjunyuan.com>
2020-07-05 18:07:05 +0800

    Create dynamic page with title prop

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,24 @@
 import React from "react";
 import { Route } from "react-router-dom";
+import { getAllPageInfor } from "../asset/pages";
 
 import Home from "../views/Home";
 import Login from "../views/Login";
+import Demo from "../views/Demo";
 import NotFound from "../views/404";
+
+const demoRoutes = getAllPageInfor().map((page) => (
+  <Route
+    exact
+    key={page.pageSlug}
+    path={page.pageSlug}
+    component={() => <Demo title={page.pageTitle} />}
+  />
+));
 
 export default [
   <Route exact key="home" path="/" component={Home} />,
   <Route exact key="login" path="/login" component={Login} />,
+  demoRoutes,
   <Route key="404" path="*" component={NotFound} />,
 ];
