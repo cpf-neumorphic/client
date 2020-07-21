d03b8381423a17585f708f23155f3ba1d61bd5cc
Kyle Huang Junyuan <kyle@hjunyuan.com>
2020-07-22 00:27:10 +0800

    Create StyledComponents outside of render method

--- src/components/Scrollable.js
+++ src/components/Scrollable.js
@@ -4,7 +4,6 @@ import styled from "styled-components";
 
 const Scrollable = ({ children }) => {
   const [width, setWidth] = useState(window.innerWidth);
-  const [parent, setParent] = useState("div");
 
   /* Get window width */
   useEffect(() => {
@@ -15,27 +14,30 @@ const Scrollable = ({ children }) => {
     };
   });
 
-  /* Set component on width change */
-  useEffect(() => {
-    if (width > 1140) setParent(Container);
-    else setParent("div");
-  }, [width]);
-
-  const ResponsiveScrollable = styled(parent)`
-    display: grid;
-    gap: 1rem;
-    grid-template-columns: repeat(3, minmax(300px, 1fr));
-    align-items: center;
-
-    /* Mobile horizontal scroll */
-    @media (max-width: 1140px) {
-      grid-auto-columns: minmax(300px, 1fr);
-      grid-auto-flow: column;
-      overflow-x: auto;
-    }
-  `;
-
-  return <ResponsiveScrollable>{children}</ResponsiveScrollable>;
+  return width > 1140 ? (
+    <ScrollableContainer>{children}</ScrollableContainer>
+  ) : (
+    <ScrollableDiv>{children}</ScrollableDiv>
+  );
 };
+
+/* Bounded by Container */
+const ScrollableContainer = styled(Container)`
+  display: grid;
+  gap: 1rem;
+  grid-template-columns: repeat(3, minmax(300px, 1fr));
+  align-items: center;
+`;
+
+/* Not bounded by Container */
+const ScrollableDiv = styled.div`
+  display: grid;
+  gap: 1rem;
+  align-items: center;
+
+  grid-auto-columns: minmax(300px, 1fr);
+  grid-auto-flow: column;
+  overflow-x: auto;
+`;
 
 export default Scrollable;
