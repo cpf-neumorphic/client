210d65a38d69661eff5c9e272f82a7b184d54166
Kyle Huang Junyuan <kyle@hjunyuan.com>
2020-07-22 00:19:53 +0800

    Conditional render of styled-component: "div"/Container

--- src/components/Scrollable.js
+++ src/components/Scrollable.js
@@ -1,17 +1,41 @@
+import React, { useState, useEffect } from "react";
+import { Container } from "react-bootstrap";
 import styled from "styled-components";
 
-export default styled.div`
-  max-width: 1140px;
-  margin: 0 auto;
-  display: grid;
-  gap: 1rem;
-  grid-template-columns: repeat(3, minmax(300px, 1fr));
-  align-items: center;
+const Scrollable = ({ children }) => {
+  const [width, setWidth] = useState(window.innerWidth);
+  const [parent, setParent] = useState("div");
 
-  /* Mobile horizontal scroll */
-  @media (max-width: 768px) {
-    grid-auto-columns: minmax(300px, 1fr);
-    grid-auto-flow: column;
-    overflow-x: auto;
-  }
-`;
+  /* Get window width */
+  useEffect(() => {
+    const handleResize = () => setWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  });
+
+  /* Set component on width change */
+  useEffect(() => {
+    if (width > 1140) setParent(Container);
+    else setParent("div");
+  }, [width]);
+
+  const ResponsiveScrollable = styled(parent)`
+    display: grid;
+    gap: 1rem;
+    grid-template-columns: repeat(3, minmax(300px, 1fr));
+    align-items: center;
+
+    /* Mobile horizontal scroll */
+    @media (max-width: 1140px) {
+      grid-auto-columns: minmax(300px, 1fr);
+      grid-auto-flow: column;
+      overflow-x: auto;
+    }
+  `;
+
+  return <ResponsiveScrollable>{children}</ResponsiveScrollable>;
+};
+
+export default Scrollable;
